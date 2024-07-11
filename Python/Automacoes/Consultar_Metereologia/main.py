861590ff66d91ebefcf252ad6b0e0240ddfcaf2a
alunogmfs2 <134213951+alunogmfs2@users.noreply.github.com>
2024-07-11 16:58:18 +0200

    Pronto

--- Python/Automacoes/Consultar_Metereologia/main.py
+++ Python/Automacoes/Consultar_Metereologia/main.py
@@ -1,4 +1,7 @@
 import requests
+import pywhatkit # type: ignore
+import pyautogui
+
 
 api_key = '2963dce9348037b72c1d3372e153adb9'
 api_url = f'https://api.openweathermap.org/data/2.5/weather'
@@ -14,4 +17,17 @@ responce = requests.get(
 
 weather_data = responce.json()
 
-print(weather_data)
+temperatura = weather_data['main']['temp']
+temperatura_minima = weather_data['main']['temp_min']
+temperatura_maxima = weather_data['main']['temp_max']
+humidade = weather_data['main']['humidity']
+
+print(f'Temperatura: {temperatura}°C')
+print(f'Temperatura mínima: {temperatura_minima}°C')
+print(f'Temperatura máxima: {temperatura_maxima}°C')
+print(f'Humidade: {humidade}%')
+
+pywhatkit.sendwhatmsg('+491772251975', f'Temperatura: {temperatura}°C, Temperatura mínima: {temperatura_minima}°C, Temperatura máxima: {temperatura_maxima}°C, Humidade: {humidade}%', 20, 10)
+pyautogui.hotkey('winleft', '2')
+pyautogui.sleep(0.1)
+pyautogui.press('enter')
